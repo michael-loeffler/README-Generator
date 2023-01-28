b7c0861ba80bbf64f614121634ff12b24708a350
mloeff <michaelloeffler23@gmail.com>
2023-01-28 17:39:02 -0600

    adds switch case structure for badges and license sections

--- index.js
+++ index.js
@@ -10,47 +10,52 @@ const questions = [
     message: 'What is the title of your project?',
     name: 'title',
 },
-{
-    type: 'input',
-    message: 'Please provide a brief description of your project:',
-    name: 'descr',
-},
-{
-    type: 'input',
-    message: 'Please provide any pertinent installation instructions you would like the user to know:',
-    name: 'install',
-},
-{
-    type: 'input',
-    message: 'Please provide usage information for your project:',
-    name: 'usage',
-},
-{
-    type: 'input',
-    message: 'Please share contribution guidelines for your project:',
-    name: 'contribution',
-},
-{
-    type: 'input',
-    message: 'Please provide instructions on how to test your project:',
-    name: 'test',
-},
+// {
+//     type: 'input',
+//     message: 'Please provide a brief description of your project:',
+//     name: 'descr',
+// },
+// {
+//     type: 'input',
+//     message: 'Please provide any pertinent installation instructions you would like the user to know:',
+//     name: 'install',
+// },
+// {
+//     type: 'input',
+//     message: 'Please provide usage information for your project:',
+//     name: 'usage',
+// },
+// {
+//     type: 'input',
+//     message: 'Please share contribution guidelines for your project:',
+//     name: 'contribution',
+// },
+// {
+//     type: 'input',
+//     message: 'Please provide instructions on how to test your project:',
+//     name: 'test',
+// },
 {
     type: 'list',
     message: 'What kind of license would you like for your project?',
     name: 'license',
-    choices: ['MIT', 'Apache', 'GPLv2', 'GPLv3', 'BSD 3-clause', 'No License'],
+    choices: ['MIT', 'Apache', 'GPLv2', 'GPLv3', 'BSD 3-clause', 'MPL 2.0', 'No License'],
 },
 {
     type: 'input',
-    message: 'What is your GitHub username?',
-    name: 'gitHub',
-},
-{
-    type: 'input',
-    message: 'What is your email address?',
-    name: 'email',
+    message: 'What is your full name?',
+    name: 'name',
 }
+// {
+//     type: 'input',
+//     message: 'What is your GitHub username?',
+//     name: 'gitHub',
+// },
+// {
+//     type: 'input',
+//     message: 'What is your email address?',
+//     name: 'email',
+// }
 ];
 
 // TODO: Create a function to write README file
@@ -66,7 +71,7 @@ function init() {
     inquirer
         .prompt(questions)
         .then((data) => {
-            const fileName = "README1.md";
+            const fileName = "README.md";
             writeToFile(fileName, data)})
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case "Apache":
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case "GPLv2":
+      return '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+    case "GPLv3":
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case "BSD 3-clause":
+      return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+    case "MPL 2.0":
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+    case "No License":
+      return '';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,11 +27,77 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license, name) {
+  switch (license) {
+    case "MIT":
+      return `MIT License
+
+      Copyright (c) [2023] [${name}]
+      
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+      
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.`;
+    case "Apache":
+      return ``;
+    case "GPLv2":
+      return ``;
+    case "GPLv3":
+      return ``;
+    case "BSD 3-clause":
+      return `BSD 3-Clause License
+
+      Copyright (c) [2023], [${name}]
+      
+      Redistribution and use in source and binary forms, with or without
+      modification, are permitted provided that the following conditions are met:
+      
+      1. Redistributions of source code must retain the above copyright notice, this
+         list of conditions and the following disclaimer.
+      
+      2. Redistributions in binary form must reproduce the above copyright notice,
+         this list of conditions and the following disclaimer in the documentation
+         and/or other materials provided with the distribution.
+      
+      3. Neither the name of the copyright holder nor the names of its
+         contributors may be used to endorse or promote products derived from
+         this software without specific prior written permission.
+      
+      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+      AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+      IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+      DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+      FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+      DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+      SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+      CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+      OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+      OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`;
+    case "MPL 2.0":
+      return ``;
+    case "No License":
+      return ``;
+    default:
+      return ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
 
   ## Description
       
@@ -38,7 +123,7 @@ function generateMarkdown(data) {
       
   ## License
       
-  ${data.license}
+  ${renderLicenseSection(data.license, data.name)}
   
   ## Contributing
       
@@ -56,12 +141,3 @@ function generateMarkdown(data) {
 module.exports = {
   generateMarkdown,
 };
-
-
-// ## Badges
-    
-// ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)    
-// Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-// If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-// The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
