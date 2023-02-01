7e90463e9b4528d92712aa74132d5d1e231b93c5
mloeff <michaelloeffler23@gmail.com>
2023-01-31 20:40:14 -0600

    adds content to renderLicenseLink function, and comments to both js files

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// TODO: Include packages needed for this application
+//-- PACKAGE DECLARATIONS --//
 const fs = require('fs');
 const inquirer = require('inquirer');
-const utils = require('./utils/generateMarkdown')
+const utils = require('./utils/generateMarkdown') // pulls the generateMarkdown module into a variable called utils  //
 
-// TODO: Create an array of questions for user input
+//-- INQUIRER PROMPTS, QUESTION ARRAY --//
 const questions = [
 {
     type: 'input',
     message: 'What is the title of your project?',
     name: 'title',
 },
-// {
-//     type: 'input',
-//     message: 'Please provide a brief description of your project:',
-//     name: 'descr',
-// },
-// {
-//     type: 'input',
-//     message: 'Please provide any pertinent installation instructions you would like the user to know:',
-//     name: 'install',
-// },
-// {
-//     type: 'input',
-//     message: 'Please provide usage information for your project:',
-//     name: 'usage',
-// },
-// {
-//     type: 'input',
-//     message: 'Please share contribution guidelines for your project:',
-//     name: 'contribution',
-// },
-// {
-//     type: 'input',
-//     message: 'Please provide instructions on how to test your project:',
-//     name: 'test',
-// },
+{
+    type: 'input',
+    message: 'Please provide a brief description of your project:',
+    name: 'descr',
+},
+{
+    type: 'input',
+    message: 'Please provide any pertinent installation instructions you would like the user to know:',
+    name: 'install',
+},
+{
+    type: 'input',
+    message: 'Please provide usage information for your project:',
+    name: 'usage',
+},
+{
+    type: 'input',
+    message: 'Please share contribution guidelines for your project:',
+    name: 'contribution',
+},
+{
+    type: 'input',
+    message: 'Please provide instructions on how to test your project:',
+    name: 'test',
+},
 {
     type: 'list',
     message: 'What kind of license would you like for your project?',
@@ -43,22 +43,18 @@ const questions = [
 },
 {
     type: 'input',
-    message: 'What is your full name?',
-    name: 'name',
+    message: 'What is your GitHub username?',
+    name: 'gitHub',
+},
+{
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
 }
-// {
-//     type: 'input',
-//     message: 'What is your GitHub username?',
-//     name: 'gitHub',
-// },
-// {
-//     type: 'input',
-//     message: 'What is your email address?',
-//     name: 'email',
-// }
 ];
 
-// TODO: Create a function to write README file
+//-- FUNCTIONS --//
+//- Creates a function that writes the README file using the generateMarkdown function from the utils module. It also logs a message stating that the file has been created. -//
 function writeToFile(fileName, data) {
     const readme = utils.generateMarkdown(data)
     
@@ -66,7 +62,7 @@ function writeToFile(fileName, data) {
     err ? console.error(err) : console.log(`Success! Your ${fileName} file has been created!`))
 }
 
-// TODO: Create a function to initialize app
+//- Creates a function that initializes the app; it is called immediately when the app is ran. -//
 function init() {
     inquirer
         .prompt(questions)
@@ -75,5 +71,5 @@ function init() {
             writeToFile(fileName, data)})
 }
 
-// Function call to initialize app
+//- Call of the init function at the outset to initialize app -//
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,7 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+//------------------------------ generateMarkdown MODULE ------------------------------//
+
+//-- FUNCTIONS --//
+//- Creates a function that returns a license badge based on which license is passed in. If there is no license, it returns an empty string. -//
 function renderLicenseBadge(license) {
   switch (license) {
     case "MIT":
@@ -20,84 +22,40 @@ function renderLicenseBadge(license) {
       return '';
   }
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license, name) {
+//- Creates a function that returns a link to a license webpage based on which license is passed in. If there is no license, it returns an empty string. -//
+function renderLicenseLink(license) {
   switch (license) {
     case "MIT":
-      return `MIT License
-
-      Copyright (c) [2023] [${name}]
-      
-      Permission is hereby granted, free of charge, to any person obtaining a copy
-      of this software and associated documentation files (the "Software"), to deal
-      in the Software without restriction, including without limitation the rights
-      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-      copies of the Software, and to permit persons to whom the Software is
-      furnished to do so, subject to the following conditions:
-      
-      The above copyright notice and this permission notice shall be included in all
-      copies or substantial portions of the Software.
-      
-      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-      SOFTWARE.`;
+      return 'https://choosealicense.com/licenses/mit/';
     case "Apache":
-      return ``;
+      return 'https://choosealicense.com/licenses/apache-2.0/';
     case "GPLv2":
-      return ``;
+      return 'https://choosealicense.com/licenses/gpl-2.0/';
     case "GPLv3":
-      return ``;
+      return 'https://choosealicense.com/licenses/gpl-3.0/';
     case "BSD 3-clause":
-      return `BSD 3-Clause License
-
-      Copyright (c) [2023], [${name}]
-      
-      Redistribution and use in source and binary forms, with or without
-      modification, are permitted provided that the following conditions are met:
-      
-      1. Redistributions of source code must retain the above copyright notice, this
-         list of conditions and the following disclaimer.
-      
-      2. Redistributions in binary form must reproduce the above copyright notice,
-         this list of conditions and the following disclaimer in the documentation
-         and/or other materials provided with the distribution.
-      
-      3. Neither the name of the copyright holder nor the names of its
-         contributors may be used to endorse or promote products derived from
-         this software without specific prior written permission.
-      
-      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-      AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-      IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-      DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-      FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-      DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-      SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-      CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-      OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-      OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`;
+      return 'https://choosealicense.com/licenses/bsd-3-clause/';
     case "MPL 2.0":
-      return ``;
+      return 'https://choosealicense.com/licenses/mpl-2.0/';
     case "No License":
-      return ``;
+      return '';
     default:
-      return ``;
+      return '';
   }
 }
-
-// TODO: Create a function to generate markdown for README
+// Creates a function that returns the license section of README using a template literal and the renderLicenseLink function. If there is no license, it returns an empty string. -//
+function renderLicenseSection(license) {
+  if (license != "No License") {
+    return `Covered under the ${license} License. For more details and to view the license in full, please visit the [${license} License Webpage](${renderLicenseLink(license)}).`
+  } else {
+    return ''
+  }
+}
+// Creates a function that generates the markdown content for README using a template literal, the renderLicenseBadge function, the renderLicenseSection function, and the 'data' object obtained from the inquirer prompts. -//
 function generateMarkdown(data) {
-  return `# ${data.title} ${renderLicenseBadge(data.license)}
+  return `# ${data.title} 
+
+  ${renderLicenseBadge(data.license)}
 
   ## Description
       
@@ -123,8 +81,8 @@ function generateMarkdown(data) {
       
   ## License
       
-  ${renderLicenseSection(data.license, data.name)}
-  
+  ${renderLicenseSection(data.license)}
+
   ## Contributing
       
   ${data.contribution}
@@ -137,7 +95,7 @@ function generateMarkdown(data) {
   
   Please visit my [GitHub profile](https://github.com/${data.gitHub}) or [send me an email with any additional questions.](mailto:${data.email})`;
 }
-
+//----- MODULARLIZES THIS FILE AND MAKES ITS METHODS ACCESSIBLE FROM OTHER JS FILES -----// 
 module.exports = {
   generateMarkdown,
 };
